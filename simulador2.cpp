1a148ccc67e825dad4eb98a88c231664f8333bce
Gustavo <guh86@hotmail.com>
2019-11-14 19:15:28 -0300

    com exp

--- simulador2.cpp
+++ simulador2.cpp
@@ -3,9 +3,10 @@
 #include <queue>
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
  
 unsigned int a = 1103515245;
-unsigned int c = 12345;
+unsigned int c = 123456;
 unsigned int m =2147483648;
 unsigned int seed = 0;
 double t;
@@ -45,7 +46,7 @@ protected:
  
 void simulation::run () {
  
-  while (! eventQueue.empty() ) { //enquanto exisitr eventos na fila de eventos 
+  while (! eventQueue.empty() ) { //enquanto exisitr eventos na fila de eventos
     event * nextEvent = eventQueue.top(); //captura evento no topo da fila
     eventQueue.pop ();  //retira evento da fila
     simtime = nextEvent->time;  //ajusta tempo de simulação
@@ -65,6 +66,10 @@ double fRand() {
 	return ((double)seed/m);
 }
 
+double exp(double lamb){
+	return (log(fRand())/(-lamb));
+}
+
 float U(){
 	seed = (a*seed+c)% m;
 	return ((float)seed/m);
@@ -72,10 +77,20 @@ float U(){
 
 class simuladorFilas : public simulation {
 public:
-	simuladorFilas() {} //aqui vai criar os estados de cada fila: ocupado, livre, etc
+	simuladorFilas() {
+		s0=s1=s2=false;
+		counter0=counter1=counter2=0;	
+		lambc0 = 0.2;
+		lambc1 = 0.3;
+		lambc2 = 0.4;
+		lambs0 = 0.3;
+		lambs1 = 0.5;
+		lambs2 = 0.6;
+	} //aqui vai criar os estados de cada fila: ocupado, livre, etc
 	std::queue<event *> aux0,aux1,aux2; //filas auxiliares
-	bool s0,s1,s2=false;
+	bool s0,s1,s2;
 	int counter0,counter1,counter2,total;
+	double lambc0,lambc1,lambc2,lambs0, lambs1, lambs2;
 
 } oSimulador;
 
@@ -84,28 +99,30 @@ public:
 	chegada0(double t) : event(t)  { }
 	virtual void processEvent ();
 };
- 
-void chegada0::processEvent() { 
-	saida0 *evs0 = new saida0(t=t+fRand());
-	oSimulador.scheduleEvent(evs0);	
-	if(oSimulador.s0 == false){
-		oSimulador.s0 = true;
-	}else{
-		oSimulador.aux0.push(evs0);
-	}
-}
+
+class saida0 : public event {
+public:
+	saida0(double t) : event(t)  { }
+	virtual void processEvent ();
+};
+
+class chegada1 : public event {
+public:
+	chegada1(double t) : event(t)  { }
+	virtual void processEvent ();
+};
 
 class saida1 : public event {
 public:
 	saida1(double t) : event(t)  { }
 	virtual void processEvent ();
 };
- 
-void saida1::processEvent() { 
-	chegada0 *evc0 = new chegada0(t=t+fRand());
-	oSimulador.scheduleEvent(evc0);	
-	//if(oSimulador.s)
-}
+
+class chegada2 : public event {
+public:
+	chegada2(double t) : event(t)  { }
+	virtual void processEvent ();
+};
 
 class saida2 : public event {
 public:
@@ -114,64 +131,94 @@ public:
 };
  
 void saida2::processEvent() { 
-	std::cout << "saida2: " << time <<'\n';
+	chegada0 *evc0 = new chegada0(t=t+exp(oSimulador.lambc0));
+	oSimulador.scheduleEvent(evc0);	
+	if(oSimulador.s0 == false) oSimulador.s0 = true;
+	else oSimulador.aux0.push(evc0);
+	if(oSimulador.aux2.empty()) oSimulador.s2 = false;
+	else oSimulador.aux2.pop();
 }
 
-class chegada2 : public event {
-public:
-	chegada2(double t) : event(t)  { }
-	virtual void processEvent ();
-};
- 
 void chegada2::processEvent() {	
-	std::cout << "chegada2: " << time <<'\n';	
+	saida2 *evs2 = new saida2(t=t+exp(oSimulador.lambs2));
+	oSimulador.scheduleEvent(evs2);	
+	if(oSimulador.s2 == false){
+		oSimulador.s2 = true;
+	}else{
+		oSimulador.aux2.push(evs2);
+	}	
+	oSimulador.counter2++;
+	oSimulador.total++;
+}
+ 
+void saida1::processEvent() { 
+	chegada0 *evc0 = new chegada0(t=t+exp(oSimulador.lambc0));
+	oSimulador.scheduleEvent(evc0);	
+	if(oSimulador.s0 == false) oSimulador.s0 = true;
+	else oSimulador.aux0.push(evc0);
+	if(oSimulador.aux1.empty()) oSimulador.s1 = false;
+	else oSimulador.aux1.pop();
 }
 
-class chegada1 : public event {
-public:
-	chegada1(double t) : event(t)  { }
-	virtual void processEvent ();
-};
- 
 void chegada1::processEvent() {
-	saida1 *evs1 = new saida1(t=t+fRand());
+	saida1 *evs1 = new saida1(t=t+exp(oSimulador.lambs1));
 	oSimulador.scheduleEvent(evs1);	
 	if(oSimulador.s1 == false){
 		oSimulador.s1 = true;
 	}else{
 		oSimulador.aux1.push(evs1);
 	}
+	oSimulador.counter1++;
+	oSimulador.total++;
 }
-
-class saida0 : public event {
-public:
-	saida0(double t) : event(t)  { }
-	virtual void processEvent ();
-};
  
 void saida0::processEvent() {
-	if(fRand() > 0.5){
-		chegada1 *evc1 = new chegada1(t=t+fRand());
+	double ii;
+	ii = fRand();
+	if(ii > 0.5){
+		std::cout << "oi1:  " << ii<< " : "<<time<<'\n';
+		chegada1 *evc1 = new chegada1(t=t+exp(oSimulador.lambc1));
 		oSimulador.scheduleEvent(evc1);
 		if(oSimulador.s1 == false) oSimulador.s1 = true;
 		else oSimulador.aux1.push(evc1);
-	}else if(fRand() < 0.3){
-		chegada2 *evc2 = new chegada2(t=t+fRand());
+	}else if(ii < 0.3){
+		std::cout << "oi2:  " <<ii<< " : "<<time<<'\n';
+		chegada2 *evc2 = new chegada2(t=t+exp(oSimulador.lambc2));
 		oSimulador.scheduleEvent(evc2);
 		if(oSimulador.s2 == false) oSimulador.s2 = true;
 		else oSimulador.aux2.push(evc2);
+	}else {
+		chegada0 *evc0 = new chegada0(t=t+exp(oSimulador.lambc0));
+		oSimulador.scheduleEvent(evc0);
+		std::cout << "oi3:  " << ii<< " : "<<time<<'\n';
 	}
 	if(oSimulador.aux0.empty()) oSimulador.s0 = false;
 	else oSimulador.aux0.pop();
 }
+ 
+void chegada0::processEvent() { 
+	if(oSimulador.total < 1000){
+		saida0 *evs0 = new saida0(t=t+exp(oSimulador.lambs0));
+		oSimulador.scheduleEvent(evs0);	
+		if(oSimulador.s0 == false){
+			std::cout << "oi0:  " << " : "<<time<< '\n';
+			oSimulador.s0 = true;
+		}else{
+			std::cout << "oi00:  " << " : "<<time<< '\n';
+			oSimulador.aux0.push(evs0);
+		}
+		oSimulador.counter0++;
+		oSimulador.total++;
+	}
+}
 
 //////////////////////// main ////////////////////////////////////////
 int main() { 
-	
-	oSimulador.scheduleEvent(new chegada0 (t=fRand()));//mudar o tempo para ser exponencial
-	oSimulador.total++;
-	std::cout << "saida2: " << oSimulador.total <<'\n';
+	oSimulador.scheduleEvent(new chegada0 (t=exp(oSimulador.lambc0)));//mudar o tempo para ser exponencial
 	oSimulador.run (); // inicia simulador	
+	std::cout << "eventos0 " << oSimulador.counter0  << '\n';
+	std::cout << "eventos1 " << oSimulador.counter1  << '\n';
+	std::cout << "eventos2 " << oSimulador.counter2  << '\n';
+	std::cout << "total " << oSimulador.total  << '\n';
 	return 0;
 }
-
